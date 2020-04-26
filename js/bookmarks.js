bf71194d1cb8e5850755840c596f9c07e771a476
Black To The Bone <blacktothebone@AirMac.local>
2020-04-26 04:33:26 +0300

    Added: fixed bookmarks bug

--- js/bookmarks.js
+++ js/bookmarks.js
@@ -1,14 +1,14 @@
 function f(n) {
     var the_id = 'id-' + n;
     var current_img = document.getElementById(the_id);
-    if (current_img.src == 'file:///Users/blacktothebone/ONE_ART/img/bookmark-icon.svg') {
-        current_img.setAttribute('src', 'file:///Users/blacktothebone/ONE_ART/img/bookmark-icon-blue.svg');
+    if (current_img.src == 'https://reizorzero.github.io/ONE_ART/img/bookmark-icon.svg') {
+        current_img.setAttribute('src', 'https://reizorzero.github.io/ONE_ART/img/bookmark-icon-blue.svg');
         var x = document.getElementById("snackbar");
         x.className = "show";
         setTimeout(function () { x.className = x.className.replace("show", ""); }, 1000);
     }
-    else if (current_img.src == 'file:///Users/blacktothebone/ONE_ART/img/bookmark-icon-blue.svg') {
-        current_img.setAttribute('src', 'file:///Users/blacktothebone/ONE_ART/img/bookmark-icon.svg');
+    else if (current_img.src == 'https://reizorzero.github.io/ONE_ART/img/bookmark-icon-blue.svg') {
+        current_img.setAttribute('src', 'https://reizorzero.github.io/ONE_ART/img/bookmark-icon.svg');
         var x = document.getElementById("snackbar-2");
         x.className = "show";
         setTimeout(function () { x.className = x.className.replace("show", ""); }, 1000);
